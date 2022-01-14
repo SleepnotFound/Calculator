416c08cde454cb4b3f5eb2c5c4b8f0ee0e9c5578
Javi <jav.olivares3@gmail.com>
2022-01-14 12:52:23 -0600

    working with negative numbers only with second value

--- script.js
+++ script.js
@@ -17,12 +17,13 @@ let last = [];
 //takes in 2 values and operator.returns 1 value
 function operate(totalArray,opr) {
     last = totalArray;
+    totalArray[0]==op? negativeParse(totalArray): null//positiveParse
     let first = totalArray.slice(0, totalArray.indexOf(opr));
     let second = totalArray.slice(totalArray.indexOf(opr)+1, totalArray.length);
-    
+
     first = parseFloat(first.join(''));
     second = parseFloat(second.join(''));
-    console.log('after parsing: ' + `${first} ` + `${opr} ` + `${second} ` )
+    console.log('after parsing: ' + `_${first}_ ` + `_${opr}_ ` + `_${second}_ ` )
     switch (opr) {
         case '+': first += second;
         break;
@@ -48,13 +49,13 @@ function inputing(event) {
     let value = event.target.id;
     switch (button) {
         case 'decimal':
-            if (!total.length || total[total.length - 1] == op) {
+            if (!total.length || total[total.length - 1] == op) {                       //at start or after an op
                 total.push('0', value)
             }
-            else if (!containsOp(total) && !total.includes('.')) {
+            else if (!op && !total.includes('.')) {                      //no op, no decimal. first 'number'
                 total.push(value)
             }
-            else if (containsOp(total) && !total.includes('.',total.indexOf(op))) {
+            else if (op && !total.includes('.',total.indexOf(op))) {     //has op,no decimal after op. second 'number'
                 total.push(value);
             }
             break;
@@ -62,26 +63,32 @@ function inputing(event) {
             total.push(value);
             break;
         case 'operator': 
-            if (!total.length) {
+            if (value == '-') {
+                containsNegative(value);
+            }
+            else if (!total.length) {                                       //lenght is 0
                 console.log('insert a number first')
             }
-            else if (total.length && !containsOp(total)) {
+            else if (total.length && !op) {                                 //lenght>0 and no op
                 op = value;
+                console.log(op)
                 total.push(value);
             }
-            else if (containsOp(total) && total[total.length-1] != op) {
+            else if (op && total[total.length-1] != op) {                   //has op and isnt last char
                 operate(total,op);
                 op = value;
                 total.push(value);
             }
             break;
         case 'operate' :
-            if (containsOp(total) && total[total.length-1] != op) {
+            if (op && total[total.length-1] != op) {                       //has op and isnt last char
                 operate(total,op)
             }
             break;
-        case 'delete' : //backspace 
-            total.pop();
+        case 'delete' :
+            let pop = total.pop();
+            pop;
+            if (pop == op) op = null;
             break;
         case 'clear':
             op = null;
@@ -91,7 +98,26 @@ function inputing(event) {
     }
 }
 
-function containsOp (total) {
-    const op = ['+','-','*','/']
-    return total.some(value => (op.includes(value)));
+function containsNegative (v) {
+    if (!total.length) {                                                //push only at begining
+        total.push(v)
+        console.log(`pushed negative at beginning`)
+    }
+    else if (op && total[total.length-1]==op) {                         //push only after op
+        total.push(v);
+        console.log('pushed negative after op')     //bug continues inserting '-'
+    }
+    else if (!op && total[total.length-1] !='-') {                      //push actual operator 
+        op = v;
+        console.log('pushed operator' + v)
+        total.push(v)
+    }
+}
+function negativeParse (array) {
+    let first = array.slice(0, array.indexOf('-',1));
+    let second = array.slice(array.indexOf('-',1)+1, array.length);
+    console.log('ng: ' + first + '_' + second)
+}
+function positiveParse (array) {
+    //last left off
 }
